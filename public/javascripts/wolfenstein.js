ccb5ebf3ab81dbaea27ec84ee4201d4a02f043e6
Sameer Singh <sameeersingh@gmail.com>
2014-01-19 10:55:40 -0800

    fix icons for adding cell above and below

--- public/javascripts/wolfenstein.js
+++ public/javascripts/wolfenstein.js
@@ -119,10 +119,10 @@ function newCellDiv(id) {
    '</div>' +
    '<div class="sidebarCell">' +
    '  <div class="btn-group btn-group-xs">' +
-   '    <button id="addAbove' + id + '" type="button" class="btn btn-default" onclick="addCellAbove(doc,' + id + ',compilers)"><i class="fa fa-chevron-up"></i></button>' +
+   '    <button id="addAbove' + id + '" type="button" class="btn btn-default" onclick="addCellAbove(doc,' + id + ',compilers)"><i class="fa fa-sort-up"></i><i class="fa fa-plus"></i></button>' +
    '    <button id="toggleEditor' + id + '" type="button" class="btn btn-default edit-btn" onclick="toggleEditor(doc,' + id + ')"><i class="fa fa-pencil fa-fw"></i></button>' +
    '    <button id="remove' + id + '" type="button" class="btn btn-default" onclick="removeCell(doc,' + id + ')"><span class="fa fa-trash-o"></span></button>' +
-   '    <button id="addBelow' + id + '" type="button" class="btn btn-default" onclick="addCellBelow(doc,' + id + ',compilers)"><i class="fa fa-chevron-down"></i></button>' +
+   '    <button id="addBelow' + id + '" type="button" class="btn btn-default" onclick="addCellBelow(doc,' + id + ',compilers)"><i class="fa fa-plus"></i><i class="fa fa-sort-down"></i></button>' +
    '  </div>' +
    '</div>' +
    '</div>'
