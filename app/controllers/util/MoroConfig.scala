60906c7d54fae953aefc88dfac1048e407aa1d93
Sameer Singh <sameeersingh@gmail.com>
2014-10-01 19:03:14 -0700

    more modular config interface

--- app/controllers/util/MoroConfig.scala
+++ app/controllers/util/MoroConfig.scala
@@ -8,7 +8,11 @@ import controllers.Compiler
  * @since 4/22/14.
  */
 class MoroConfig(underlying: Configuration) {
-  def editor = underlying.getBoolean("editor").getOrElse(true)
-
   def config(c: Compiler): Option[Configuration] = underlying.getConfig("compilers." + c.name)
+
+  def viewConfig(name: String): Option[Configuration] = underlying.getConfig("views." + name)
+
+  def editorConfig = viewConfig("editor")
+  def editorEnabled = editorConfig.map(c => c.getBoolean("enabled").getOrElse(true)).getOrElse(true)
+  def editorPassHash = editorConfig.map(c => c.getString("passWordHash").getOrElse("")).getOrElse("")
 }
